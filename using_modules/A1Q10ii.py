37b91707b0e96d317c826907a1085ac157de6195
Manvi Mehendiyan <79982384+manvimehendiyan@users.noreply.github.com>
2022-08-23 22:35:13 +0530

    Update and rename A1Q10ii.py to main.py

--- using_modules/A1Q10ii.py
+++
@@ -1,6 +0,0 @@
-import A1Q10i as module
-
-module.amount
-module.rate
-module.time
-module.interest(module.amount,module.rate,module.time)
---
+++ using_modules/main.py
@@ -0,0 +1,6 @@
+import compound_interest as module
+
+module.amount
+module.rate
+module.time
+module.interest(module.amount,module.rate,module.time)
