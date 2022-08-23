d969fbd028a0d4a8bc90db8f0f4c3a93e7637f6c
Manvi Mehendiyan <79982384+manvimehendiyan@users.noreply.github.com>
2022-08-23 22:32:20 +0530

    Delete A1Q10i.py

--- A1Q10i.py
+++
@@ -1,8 +0,0 @@
-def interest(P,R,T):
-    return (P*R*T)/100
-
-amount=int(input("Enter the Principal Amount: "))
-rate=int(input("Enter the Interest Rate(in percentage): ")) 
-time=int(input("Enter the time(in years): "))
-
-print("The compound interest is ",interest(amount,rate,time))
